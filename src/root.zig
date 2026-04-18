c4a403ee94297343e9e78f02a221344650a49c21
atcoun <atcoun@gmail.com>
2026-04-18 20:14:59 +0500

    Create root.zig

---
+++ src/root.zig
@@ -0,0 +1,5 @@
+pub const Bot = @import("client/bot.zig").Bot;
+pub const ClientSession = @import("client/session/http_client.zig").ClientSession;
+pub const enums = @import("enums.zig");
+pub const types = @import("types.zig");
+pub const errors = @import("errors.zig");
