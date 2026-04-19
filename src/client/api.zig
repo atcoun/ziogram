d0b6ccca47cb24a7a02a8ac103374b73fac79980
atcoun <atcoun@gmail.com>
2026-04-20 03:12:09 +0500

    Update and rename endpoint.zig to api.zig

--- src/client/api.zig
+++ src/client/api.zig
@@ -8,20 +8,11 @@ pub const FilesPathWrapper = union(enum) {
     },
 
     pub fn toLocal(self: @This(), allocator: std.mem.Allocator, path: []const u8) ![]const u8 {
-        return self.transform(allocator, path, true);
-    }
-
-    pub fn toServer(self: @This(), allocator: std.mem.Allocator, path: []const u8) ![]const u8 {
-        return self.transform(allocator, path, false);
-    }
-
-    fn transform(self: @This(), allocator: std.mem.Allocator, path: []const u8, local: bool) ![]const u8 {
         if (self == .bare) return try allocator.dupe(u8, path);
         const s = self.simple;
-        const from, const to = if (local) .{ s.server_path, s.local_path } else .{ s.local_path, s.server_path };
 
-        if (std.mem.cutPrefix(u8, path, from)) |relative|
-            return try std.fs.path.resolve(allocator, &.{ to, relative });
+        if (std.mem.cutPrefix(u8, path, s.server_path)) |relative|
+            return try std.fs.path.resolve(allocator, &.{ s.local_path, relative });
         return try allocator.dupe(u8, path);
     }
 };
@@ -47,12 +38,13 @@ fn format(self: @This(), allocator: std.mem.Allocator, template: []const u8, k1:
     return try std.fmt.allocPrint(allocator, "{s}{s}{s}{s}{s}", .{ c1.@"0", v1, c2.@"0", v2, c2.@"1" });
 }
 
-pub fn fromBase(allocator: std.mem.Allocator, base_url: []const u8, is_local: bool) !@This() {
+pub fn fromBase(allocator: std.mem.Allocator, base_url: []const u8, is_local: bool, wrap_local_file: ?FilesPathWrapper) !@This() {
     const root = std.mem.trimEnd(u8, base_url, "/");
     return .{
         .base = try std.fmt.allocPrint(allocator, "{s}/bot{{token}}/{{method}}", .{root}),
         .file = try std.fmt.allocPrint(allocator, "{s}/file/bot{{token}}/{{path}}", .{root}),
         .is_local = is_local,
+        .wrap_local_file = wrap_local_file orelse .bare,
         .is_owned = true,
     };
 }
