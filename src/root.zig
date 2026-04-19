6cf96664703cb02e94375406cddf542f3dc661e2
atcoun <atcoun@gmail.com>
2026-04-20 03:14:48 +0500

    Update root.zig

--- src/root.zig
+++ src/root.zig
@@ -1,8 +1,8 @@
+pub const TelegramAPI = @import("client/api.zig");
 pub const Bot = @import("client/bot.zig");
 pub const Client = @import("client/http.zig");
-pub const Endpoint = @import("client/endpoint.zig");
+
 pub const enums = @import("enums");
 pub const errors = @import("errors");
 pub const methods = @import("methods");
 pub const types = @import("types");
-pub const ZiogramError = errors.ZiogramError;
