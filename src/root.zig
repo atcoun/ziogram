ccc3fd42bb9f4097e20efbb2cd5995fb943b004a
atcoun <atcoun@gmail.com>
2026-04-19 19:32:43 +0500

    Update root.zig

--- src/root.zig
+++ src/root.zig
@@ -1,4 +1,5 @@
 pub const Bot = @import("client/bot.zig").Bot;
+pub const TelegramAPI = @import("client/telegram_api.zig").TelegramAPI;
 pub const ClientSession = @import("client/session/http_client.zig").ClientSession;
 pub const enums = @import("enums.zig");
 pub const types = @import("types.zig");
