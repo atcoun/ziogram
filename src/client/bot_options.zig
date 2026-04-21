f4c60e323ec8ad87602b4494d509cf0ea461846a
atcoun <atcoun@gmail.com>
2026-04-21 10:42:42 +0500

    Update bot_options.zig

--- src/client/bot_options.zig
+++ src/client/bot_options.zig
@@ -31,3 +31,43 @@ pub fn resolve(self: *@This()) void {
         };
     }
 }
+
+test "resolve does nothing when link_preview_options already set" {
+    var opts = @This(){
+        .link_preview_options = .{ .is_disabled = true },
+        .link_preview_is_disabled = true,
+    };
+    opts.resolve();
+
+    try std.testing.expect(opts.link_preview_options != null);
+    try std.testing.expect(opts.link_preview_options.?.is_disabled == true);
+}
+
+test "resolve creates link_preview_options from is_disabled" {
+    var opts = @This(){
+        .link_preview_is_disabled = true,
+    };
+    opts.resolve();
+
+    try std.testing.expect(opts.link_preview_options != null);
+    try std.testing.expect(opts.link_preview_options.?.is_disabled == true);
+}
+
+test "resolve creates link_preview_options from prefer_small_media" {
+    var opts = @This(){
+        .link_preview_prefer_small_media = true,
+    };
+    opts.resolve();
+
+    try std.testing.expect(opts.link_preview_options != null);
+    try std.testing.expect(opts.link_preview_options.?.prefer_small_media == true);
+}
+
+test "resolve does nothing when no link preview fields set" {
+    var opts = @This(){
+        .parse_mode = .HTML,
+    };
+    opts.resolve();
+
+    try std.testing.expect(opts.link_preview_options == null);
+}
