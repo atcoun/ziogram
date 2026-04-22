d79dd68894c9f69b19344c6a4bf92f801bfc9f62
atcoun <atcoun@gmail.com>
2026-04-23 01:42:49 +0500

    Update http_options.zig

--- src/client/http_options.zig
+++ src/client/http_options.zig
@@ -3,5 +3,6 @@ const http = std.http;
 
 const TelegramAPI = @import("../client/api.zig");
 
+pool_size: usize = 100,
 api: TelegramAPI = TelegramAPI.PRODUCTION,
 proxy: ?*const http.Client.Proxy = null,
