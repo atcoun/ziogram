762f53f68559958ac82bfe70432b439c46c51f6e
atcoun <atcoun@gmail.com>
2026-04-23 02:38:39 +0500

    Update http_options.zig

--- src/client/http_options.zig
+++ src/client/http_options.zig
@@ -3,7 +3,6 @@ const http = std.http;
 
 const TelegramAPI = @import("../client/api.zig");
 
-pool_size: usize = 100,
 read_buffer_size: usize = 8192,
 write_buffer_size: usize = 1024,
 api: TelegramAPI = TelegramAPI.PRODUCTION,
